22e1d5ddbc7b3ed37ba5a7a68b8a1eb94c3f1489
Judygubb <judy123mody@gmail.com>
2026-02-14 18:18:25 +0100

    Backend implementation
    Make the start, pause and reset buttons functional

--- src/Bootstrappers/main.js
+++
@@ -1,8 +0,0 @@
-// This the the entry point --> index.jsx
-
-import { createApp } from "vue";
-import VueRoot from "./VueRoot.vue";  
-
-console.log("Logging: main");  // Logging
-
-createApp(VueRoot).mount("#root");
